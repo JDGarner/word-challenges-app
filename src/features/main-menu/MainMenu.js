3c65f2159dc9498e3d6cb9e0fac493e3fe0692c1
jamieg <jamie.garner@caplin.com>
2020-01-05 19:46:23 +0000

    initial work on menu

--- src/features/main-menu/MainMenu.js
+++ src/features/main-menu/MainMenu.js
@@ -5,24 +5,44 @@ import { LargeText, AppBackground, PaddedButton } from "../../components";
 
 const MenuContainer = styled(View)`
   flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const MenuItems = styled(View)`
+  flex: 1;
+  width: 100%;
   justify-content: center;
   align-items: center;
 `;
 
 const MenuItem = styled(PaddedButton)`
-  width: 100%;
+  width: 240;
+  align-items: center;
+  margin-vertical: 24;
+`;
+
+const Title = styled(View)`
+  height: 30%;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 const MainMenu = ({ navigation }) => {
   return (
     <AppBackground theme="menu">
       <MenuContainer>
-        <MenuItem onPress={() => navigation.navigate("DefinitionGame")}>
-          <LargeText>Definitions</LargeText>
-        </MenuItem>
-        <MenuItem onPress={() => navigation.navigate("RhymeGame")}>
-          <LargeText>Rhymes</LargeText>
-        </MenuItem>
+        <Title>
+          <LargeText>Word Binge</LargeText>
+        </Title>
+        <MenuItems>
+          <MenuItem onPress={() => navigation.navigate("DefinitionGame")}>
+            <LargeText>Definitions</LargeText>
+          </MenuItem>
+          <MenuItem onPress={() => navigation.navigate("RhymeGame")}>
+            <LargeText>Rhymes</LargeText>
+          </MenuItem>
+        </MenuItems>
       </MenuContainer>
     </AppBackground>
   );
