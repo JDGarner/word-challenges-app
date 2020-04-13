946e4d5365917e667efbe624d99c534b78cadd5d
Jamie Garner <jamiegarner123@gmail.com>
2020-04-13 15:59:33 +0800

    add dev and prod config for ios

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,9 @@
 import Config from "react-native-config";
+import { Platform } from "react-native";
 
 export const MOCK_URL = "MOCK";
-export const LOCAL_URL = "http://localhost:3001";
+export const LOCAL_URL =
+  Platform.OS === "android" ? "http://10.0.2.2:3001" : "http://localhost:3001";
 
 let ConfigWithOverrides = {
   ...Config,
