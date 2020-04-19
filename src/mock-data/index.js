4383d688f9dee5d5e6fac31cfa01bbef4c705cf7
Jamie Garner <jamiegarner123@gmail.com>
2020-04-19 20:24:16 +0800

    update mock data

--- src/mock-data/index.js
+++ src/mock-data/index.js
@@ -1,18 +1,14 @@
 import { ENDPOINTS } from "../app-constants";
 import rhymes from "./rhymes";
-import { easyDefinitions, hardDefinitions } from "./definitions";
+import { definitions } from "./definitions";
 
 const getMockData = endpoint => {
   if (endpoint === ENDPOINTS.RHYMES) {
     return rhymes;
   }
 
-  if (endpoint === ENDPOINTS.EASY_DEFINITIONS) {
-    return easyDefinitions;
-  }
-
-  if (endpoint === ENDPOINTS.HARD_DEFINITIONS) {
-    return hardDefinitions;
+  if (endpoint === ENDPOINTS.DEFINITIONS) {
+    return definitions;
   }
 
   return null;
