dcf1b7c12d591793a03e791969e0a3f19e469d3e
Jamie Garner <jamiegarner123@gmail.com>
2020-05-04 12:23:10 +0800

    more game center integration

--- src/redux/leaderboard-services/game-center-middleware.js
+++ src/redux/leaderboard-services/game-center-middleware.js
@@ -5,40 +5,56 @@ import {
   GAME_CENTER_INIT,
   SUBMIT_SCORE,
   submitScoreToLeaderboard,
+  gameCenterInit,
+  SHOW_LEADERBOARD,
 } from "./leaderboard-services-actions";
 import { getELOKeysForMode } from "../../utils/elo-utils";
 import { getConfig } from "../../Config";
 
+// TODO: submit score then show leaderboard, init if any of those fail
+const showLeaderboards = () => {};
+
 export default store => next => action => {
   switch (action.type) {
     case SHOW_ALL_LEADERBOARDS:
-      GameCenter.openLeaderboardModal()
-        .then(() => {
-          console.log("Game Center: Open Leaderboard Successful");
+      GameCenter.openLeaderboards({
+        leaderboardIdentifier: LEADERBOARD_IDS.DEFINITIONS,
+      })
+        .then(res => {
+          console.log("Game Center: Open Leaderboard Successful. ", res);
+          store.dispatch(submitScoreToLeaderboard(MODES.DEFINITIONS));
+          store.dispatch(submitScoreToLeaderboard(MODES.RHYMES));
         })
-        .catch(() => {
-          console.log("Game Center: Open Leaderboard Failed");
+        .catch(res => {
+          console.log("Game Center: Open Leaderboard Failed. ", res);
+          store.dispatch(gameCenterInit());
         });
-
-      store.dispatch(submitScoreToLeaderboard(MODES.DEFINITIONS));
-      store.dispatch(submitScoreToLeaderboard(MODES.RHYMES));
 
       break;
 
-    // case SHOW_LEADERBOARD:
+    case SHOW_LEADERBOARD:
+      GameCenter.openLeaderboardModal({
+        leaderboardIdentifier: action.id,
+      })
+        .then(res => {
+          console.log("Game Center: Open Leaderboard Successful. ", res);
+          store.dispatch(submitScoreToLeaderboard(MODES.DEFINITIONS));
+          store.dispatch(submitScoreToLeaderboard(MODES.RHYMES));
+        })
+        .catch(res => {
+          console.log("Game Center: Open Leaderboard Failed. ", res);
+          store.dispatch(gameCenterInit());
+        });
 
-    //   store.dispatch(submitScoreToLeaderboard(MODES.DEFINITIONS));
-    //   store.dispatch(submitScoreToLeaderboard(MODES.RHYMES));
-
-    //   break;
+      break;
 
     case GAME_CENTER_INIT:
       GameCenter.init({ leaderboardIdentifier: LEADERBOARD_IDS.DEFINITIONS })
-        .then(() => {
-          console.log("Game Center: Init Successful");
+        .then(res => {
+          console.log("Game Center: Init Successful. ", res);
         })
-        .catch(() => {
-          console.log("Game Center: Init Failed");
+        .catch(res => {
+          console.log("Game Center: Init Failed. ", res);
         });
 
       break;
@@ -49,16 +65,17 @@ export default store => next => action => {
       const scoreToSubmit = action.score || eloTracking[stateKey];
 
       const { IS_PROD } = getConfig();
+      // TODO: take out true
       if (IS_PROD === "true" || true) {
         GameCenter.submitLeaderboardScore({
           score: scoreToSubmit,
-          leaderboarIdentifier: leaderboardId,
+          leaderboardIdentifier: leaderboardId,
         })
-          .then(() => {
-            console.log("Game Center: Score Submit Successful");
+          .then(res => {
+            console.log("Game Center: Score Submit Successful. ", res);
           })
-          .catch(() => {
-            console.log("Game Center: Score Submit Failed");
+          .catch(res => {
+            console.log("Game Center: Score Submit Failed. ", res);
           });
       } else {
         console.log(">>> DEV: Submitting ", scoreToSubmit, " to Game Center");
